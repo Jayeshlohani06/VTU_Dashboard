68082d829b36b50ae039186cc289e39ce7d7822e
Jayeshlohani06 <jayeshlohani06@gmail.com>
2025-10-17 15:42:55 +0530

    Change app.run to app.run_server with host and port

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ server = app.server
 
 # ----------------- Run App -----------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(host='0.0.0.0', port=10000, debug=True)
